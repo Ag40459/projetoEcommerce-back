627c5d546f31d8a0ad511eacd575a91e95f89aff
Agenor Torres <agenorpoker@gmail.com>
2023-05-04 08:25:10 -0300

    modificado dentro do controlador account em getAccountID para que registre qualquer movimentação

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -24,32 +24,6 @@ const updateAccount = async (req, res) => {
 
         const updatedAccount = {};
 
-        if (balance) updatedAccount.balance = balance;
-        if (deposit_pending) updatedAccount.deposit_pending = deposit_pending;
-        if (deposit_confirmed) updatedAccount.deposit_confirmed = deposit_confirmed;
-        if (bonus) updatedAccount.bonus = bonus;
-
-        await knex("accounts").where({ id }).update(updatedAccount);
-
-        return res.status(200).json({ message: "Conta atualizada com sucesso" });
-    } catch (error) {
-        return res.status(500).json({ message: "Erro ao atualizar conta", error: error.message });
-    }
-};
-
-const getAccountId = async (req, res) => {
-    const { id } = req.params;
-    const { balance, deposit_pending, deposit_confirmed, bonus } = req.body;
-
-    try {
-        const accountExist = await knex("accounts").where({ id, deleted: false }).first();
-
-        if (!accountExist) {
-            return res.status(404).json({ message: "Conta não encontrada" });
-        }
-
-        const updatedAccount = {};
-
         if (balance) {
             updatedAccount.balance = balance;
             await knex('accounts')
@@ -93,6 +67,38 @@ const getAccountId = async (req, res) => {
         return res.status(500).json({ message: "Erro ao atualizar conta", error: error.message });
     }
 
+};
+
+const getAccountId = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const account = await knex("accounts")
+            .where({ id, deleted: false })
+            .select([
+                "id",
+                "user_id",
+                "balance",
+                "deposit_pending",
+                "deposit_confirmed",
+                "bonus",
+                "last_deposit",
+                "last_withdrawal",
+                "last_bonus",
+                "created_at",
+            ])
+            .first();
+
+        if (!account) {
+            return res.status(404).json({ message: "Conta não encontrada" });
+        }
+
+        return res.status(200).json({ message: "Conta encontrada", account });
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ message: "Erro ao buscar conta", error: error.message });
+    }
 };
 
 

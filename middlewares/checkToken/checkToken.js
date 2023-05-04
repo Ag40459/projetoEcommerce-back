a6a9f9265036d730f8321fa9f5be6ba6b9780ee6
Agenor Torres <agenorpoker@gmail.com>
2023-05-04 09:14:35 -0300

    adicionado o middleware

--- middlewares/checkToken/checkToken.js
+++ middlewares/checkToken/checkToken.js
@@ -1,6 +1,24 @@
 const jwt = require("jsonwebtoken");
 const { secret } = require("../config/auth");
 
+const authMiddleware = async (req, res, next) => {
+    const { authorization } = req.headers;
+    if (!authorization || typeof authorization !== "string" || !authorization.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "Token inválido ou não fornecido." });
+    }
+    const token = authorization.split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(token, secret);
+        req.user = { id: decoded.id }; // define req.user com o id do usuário
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Token inválido" });
+    }
+};
+
+module.exports = authMiddleware;
+
 // const checkToken = async (req, res, next) => {
 //     const { authorization } = req.headers;
 
@@ -34,23 +52,4 @@ const { secret } = require("../config/auth");
 // module.exports = checkToken;
 
 
-const authMiddleware = async (req, res, next) => {
-    const token = req.headers.authorization;
 
-    if (!token) {
-        return res.status(401).json({ message: "Token não fornecido" });
-    }
-
-    try {
-        const decoded = jwt.verify(token, secret);
-        req.user = { id: decoded.id }; // define req.user com o id do usuário
-        next();
-    } catch (error) {
-        return res.status(401).json({ message: "Token inválido" });
-    }
-};
-
-module.exports = authMiddleware;
-
-
-module.exports = authMiddleware;

294500922bc60b3b61d9b5b403a6c9709b1c3620
Agenor Torres <agenorpoker@gmail.com>
2023-05-04 08:06:18 -0300

    modificado dentro do controlador account em getAccountID para que registre qualquer movimentação

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -39,34 +39,60 @@ const updateAccount = async (req, res) => {
 
 const getAccountId = async (req, res) => {
     const { id } = req.params;
+    const { balance, deposit_pending, deposit_confirmed, bonus } = req.body;
 
     try {
-        const account = await knex("accounts")
-            .where({ id, deleted: false })
-            .select([
-                "id",
-                "user_id",
-                "balance",
-                "deposit_pending",
-                "deposit_confirmed",
-                "bonus",
-                "last_deposit",
-                "last_withdrawal",
-                "last_bonus",
-                "created_at",
-            ])
-            .first();
+        const accountExist = await knex("accounts").where({ id, deleted: false }).first();
 
-        if (!account) {
+        if (!accountExist) {
             return res.status(404).json({ message: "Conta não encontrada" });
         }
 
-        return res.status(200).json({ message: "Conta encontrada", account });
+        const updatedAccount = {};
+
+        if (balance) {
+            updatedAccount.balance = balance;
+            await knex('accounts')
+                .where({ id })
+                .update({
+                    transfer_history: knex.raw(`JSONB_INSERT(transfer_history, '{movements}', '{"balance": ${balance}, "date": "${new Date().toISOString()}"}')`)
+                })
+        }
+
+        if (deposit_pending) {
+            updatedAccount.deposit_pending = deposit_pending;
+            await knex('accounts')
+                .where({ id })
+                .update({
+                    transfer_history: knex.raw(`JSONB_INSERT(transfer_history, '{movements}', '{"deposit_pending": ${deposit_pending}, "date": "${new Date().toISOString()}"}')`)
+                })
+        }
+
+        if (deposit_confirmed) {
+            updatedAccount.deposit_confirmed = deposit_confirmed;
+            await knex('accounts')
+                .where({ id })
+                .update({
+                    transfer_history: knex.raw(`JSONB_INSERT(transfer_history, '{movements}', '{"deposit_confirmed": ${deposit_confirmed}, "date": "${new Date().toISOString()}"}')`)
+                })
+        }
+
+        if (bonus) {
+            updatedAccount.bonus = bonus;
+            await knex('accounts')
+                .where({ id })
+                .update({
+                    transfer_history: knex.raw(`JSONB_INSERT(transfer_history, '{movements}', '{"bonus": ${bonus}, "date": "${new Date().toISOString()}"}')`)
+                })
+        }
+
+        await knex("accounts").where({ id }).update(updatedAccount);
+
+        return res.status(200).json({ message: "Conta atualizada com sucesso" });
     } catch (error) {
-        return res
-            .status(500)
-            .json({ message: "Erro ao buscar conta", error: error.message });
+        return res.status(500).json({ message: "Erro ao atualizar conta", error: error.message });
     }
+
 };
 
 
